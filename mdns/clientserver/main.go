dc7acd75f05dc4fcf777cd15f234eb022c6d4a51
SteveOC <steven.oconnor@neds.com>
2018-08-04 11:35:19 +1000

    default to rand

--- mdns/clientserver/main.go
+++ mdns/clientserver/main.go
@@ -2,34 +2,59 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/micro/mdns"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		panic("Usage: clientserver name-of-this-server")
+	rand.Seed(time.Now().UnixNano())
+	serviceName := "myService"
+	if len(os.Args) == 2 {
+		serviceName = os.Args[1]
+	} else {
+		serviceName = fmt.Sprintf("Random%06d", rand.Intn(999999))
 	}
 	// Setup our service export
 	host, _ := os.Hostname()
-	serviceName := os.Args[1]
-	fmt.Println("Running", serviceName)
 	info := []string{"My awesome service"}
-	service, _ := mdns.NewMDNSService(host, serviceName, "", "", 8000, nil, info)
+	port := 8000 + rand.Intn(10)
+	port = 8000
+	fmt.Println("Running", serviceName, port)
+	service, err := mdns.NewMDNSService(host, serviceName, "", "", port, nil, info)
+	if err != nil {
+		fmt.Println("error creating service", err.Error())
+	}
 
 	// Create the mDNS server, defer shutdown
-	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+	server, err := mdns.NewServer(&mdns.Config{Zone: service})
+	if err != nil {
+		fmt.Println("error starting server", err.Error())
+	}
 	defer server.Shutdown()
 	// Make a channel for results and start listening
 	entriesCh := make(chan *mdns.ServiceEntry, 4)
+	others := map[string]string{}
 	go func() {
 		for entry := range entriesCh {
-			fmt.Printf("%s Got new entry: %v\n", serviceName, entry)
+			if !strings.Contains(entry.Name, serviceName) {
+				fmt.Printf("%s Got new entry: %v\n", serviceName, entry.Name)
+				others[entry.Name] = entry.Host
+			}
 		}
 	}()
 
 	// Start the lookup
 	mdns.Lookup("_foobar._tcp", entriesCh)
+	for {
+		fmt.Println("................")
+		time.Sleep(time.Second * 2)
+		for k, _ := range others {
+			fmt.Println(">>", serviceName, "knows", k)
+		}
+	}
 	close(entriesCh)
 }
